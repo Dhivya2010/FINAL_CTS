211f3519c93cc46720d1784d54475152ac7bf0ff
Dhivya2010 <164025202+Dhivya2010@users.noreply.github.com>
2024-08-16 18:55:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -87,16 +87,34 @@ if st.button('Predict'):
         st.write("TOTAL WEEKS FOR CTT-FSI:", y_pred1[0] + y_pred2[0], "Weeks")
 
         total_weeks = y_pred1[0] + y_pred2[0]
-
         if total_weeks <= 105:
             st.subheader("High Demand")
-            st.write("Site has significantly shorter predicted readiness time. They are at higher risk of delays and require close monitoring to ensure timelines are met.")
+            st.write("Resource Allocation:This site requires Allocation of additional personnel and equipment due to the high priority")
+            st.write("Risks Identified:")
+            st.write("- Potential delays due to rapid setup")
+            st.write("Mitigation Strategies:")
+            st.write("- Provide additional support and resources")
+            st.write("- Implement expedited processes to align with the",y_pred1[0] + y_pred2[0],"weeks timeline")
         elif total_weeks <= 118:
             st.subheader("Medium Demand")
-            st.write("Site has readiness time close to the average. They are on track but should be monitored to maintain progress.")
+            st.write("Resource Allocation:This site requires Allocation of balanced amount of personnel and equipment. Ensure resources are sufficient but avoid over-allocation")
+            st.write("Risks Identified:")
+            st.write("-Risk of insufficient resources if allocation is not balanced.")
+            st.write("-Pressure to meet the timeline can lead to scheduling conflicts or delays.")
+            st.write("Mitigation Strategies:")
+            st.write("-Distribute resources evenly to ensure all sites are adequately supported.")
+            st.write("-Ensure that site readiness and setup processes are efficiently managed within the",y_pred1[0] + y_pred2[0]," weeks timeframe.")
         else:
             st.subheader("Low Demand")
-            st.write("Sites has longer predicted readiness time. They are ahead of schedule and is expected to meet milestones without significant delays.")
+            st.write("Resource Allocation:This site requires Minimal additional resources or only Maintenance of existing resource levels as the site is ahead of schedule.")
+            st.write("Risks Identified:")
+            st.write("-Extended preparation period may result in delays if not managed carefully.")
+            st.write("-Potential for delays in resource availability due to phased allocation.")
+            st.write("Mitigation Strategies:")
+            st.write("-Continuously monitor progress and adjust plans as needed to prevent any emerging issues from causing delays.")
+            st.write("-Implement long-term planning and monitoring to ensure readiness within the",y_pred1[0] + y_pred2[0],"weeks timeframe")
+
+
 
     except Exception as e:
         st.error(f"Error making predictions: {e}")
